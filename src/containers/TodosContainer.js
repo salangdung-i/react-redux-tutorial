6f07937b26481052eafc0b0dff9b68fbeda29ce4
salangdung-i <dladmswl92@naver.com>
2022-04-04 10:24:05 +0900

    useActions 만들어 적용하기

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -2,16 +2,17 @@ import React, { useCallback } from 'react';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/todos'
 import { connect, useDispatch, useSelector } from 'react-redux';
+import useActions from '../lib/useActions';
 const TodosContainer = () => {
   const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
-  const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
 
   return (
     <Todos
@@ -25,4 +26,4 @@ const TodosContainer = () => {
   );
 };
 
-export default TodosContainer;
+export default React.memo(TodosContainer);
